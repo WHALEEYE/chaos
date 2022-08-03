06770aaebb00911897e3718baa4fe5e7a3b13368
Isaac Jin <1094349973@qq.com>
2022-08-03 11:09:24 +0800

    color adjusted

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,12 @@ export default function Home() {
                     Welcome to Chaos.
                 </h1>
 
+                <font className={styles.subtitle}>The End of My Cosmic Mind</font>
+
                 <Script src="https://cdn.logwork.com/widget/countdown.js" strategy={"afterInteractive"}></Script>
                 <a href="https://logwork.com/countdown-79iv" className="countdown-timer" data-style="circles"
-                   data-timezone="Asia/Shanghai" data-date="2022-08-24 23:59" data-background="#00f9ee">Blog Will be
-                    Open In</a>
+                   data-timezone="Asia/Shanghai" data-date="2022-08-24 23:59" data-background="#00f9ee" data-unitscolor="#ababab"
+                   data-textcolor="#ababab">Open In</a>
 
 
                 {/*<p className={styles.description}>*/}

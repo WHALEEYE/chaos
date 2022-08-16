42efaa32eb3394c98e54d5aead83d200009ca956
Isaac Jin <1094349973@qq.com>
2022-08-16 13:56:21 +0800

    changed to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Script from "next/script";
+
+const Home: NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Chaos</title>
+                <meta name="description" content="The end of the universe in my mind"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to Chaos.
+                </h1>
+
+                <h1 className={styles.subtitle}>The End of My Cosmic Mind</h1>
+
+                <Script src={"https://cdn.logwork.com/widget/countdown.js"} strategy={"afterInteractive"}></Script>
+                <a href="https://logwork.com/countdown-79iv" className="countdown-timer" data-style="circles"
+                   data-timezone="Asia/Shanghai" data-date="2022-08-24 23:59" data-background="#00f9ee"
+                   data-unitscolor="#ababab"
+                   data-textcolor="#ababab">Open In</a>
+            </main>
+        </div>
+    )
+}
+
+export default Home
